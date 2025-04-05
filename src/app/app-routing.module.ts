f3cc7936cb24c028a8eb988b940f311fc3c482f2
Jose Gamaliel Potenciano Mendez <gmen22587@gmail.com>
2025-04-04 21:47:49 -0600

    Agregado cors a las dependencias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,20 @@ import { AuthComponent } from './auth.component';
 import { PeliculaComponent } from './pelicula.component';
 
 const routes: Routes = [
+  // Redirige la raíz al componente 'cartelera' por defecto
   { path: '', redirectTo: '/cartelera', pathMatch: 'full' },
+  
+  // Ruta para mostrar el componente Cartelera
   { path: 'cartelera', component: CarteleraComponent },
+  
+  // Ruta para el login (puede ser pública para que los usuarios accedan)
   { path: 'login', component: AuthComponent },
-  { path: 'admin', component: PeliculaComponent }
+  
+  // Ruta para el panel de administración (requiere autenticación, por ejemplo)
+  { path: 'admin', component: PeliculaComponent },
+  
+  // Ruta wildcard (por si hay rutas no definidas)
+  { path: '**', redirectTo: '/cartelera', pathMatch: 'full' }
 ];
 
 @NgModule({
